f4ac92a54a6c5a84b258c4986c5eb06d52ecd445
Wakaba <w@suika.fam.cx>
2009-12-28 22:30:14 +0900

    removed cvs-related files

--- cvscommit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-find -name ChangeLog | xargs cvs diff | grep "^\+" | sed -e "s/^\+//; s/^\+\+ .\//++ html\//" > .cvslog.tmp
-cvs commit -F .cvslog.tmp $1 $2 $3 $4 $5 $6 $7 $8 $9 
-rm .cvslog.tmp
-
-## $Date: 2007/05/01 09:25:13 $
-## License: Public Domain
